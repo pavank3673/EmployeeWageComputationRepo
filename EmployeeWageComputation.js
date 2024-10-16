f4a32768c8ed4766f267b3cc165b13222e10dd09
pavank3673 <pavank3673@outlook.com>
2024-10-16 17:01:08 +0530

    initial commit

---
+++ EmployeeWageComputation.js
@@ -0,0 +1,35 @@
+let employeeList = [{}];
+
+employeeList[0] = {
+  emp_id: 101,
+  emp_name: "sam",
+  comp_name: "Comm",
+};
+
+employeeList[1] = {
+  emp_id: 102,
+  emp_name: "adam",
+  comp_name: "Telmn",
+};
+
+employeeList[2] = {
+  emp_id: 103,
+  emp_name: "roger",
+  comp_name: "Comm",
+};
+
+employeeList[3] = {
+  emp_id: 104,
+  emp_name: "monte",
+  comp_name: "Telmn",
+};
+
+employeeList[4] = {
+  emp_id: 106,
+  emp_name: "kassing",
+  comp_name: "Comm",
+};
+
+function menuDisplay() {
+  console.log("Welcome to Employee Wage Computation Program");
+}
